14e6b353e4ce604d4791ac36d12c052ac2554349
Sadman Adib <sadmanadib67@gmail.com>
2023-07-17 22:30:22 +0600

    Initial Commit

---
+++ PaswordSaver/PaswordSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaswordSaverApp.swift
+//  PaswordSaver
+//
+//  Created by Sadman Adib on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PaswordSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
